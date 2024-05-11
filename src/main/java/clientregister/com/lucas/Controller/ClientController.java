9ac824352e792e643712ec1dc6dbbc3b37f1912f
LukasPio <lucaspio.galvao@gmail.com>
2024-05-11 11:05:21 -0300

    Create save/update routes

--- src/main/java/clientregister/com/lucas/Controller/ClientController.java
+++
@@ -1,9 +0,0 @@
-package clientregister.com.lucas.Controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(path = "/api/client")
-public class ClientController {
-}

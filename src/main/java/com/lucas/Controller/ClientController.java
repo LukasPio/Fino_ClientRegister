fa6518590aaaa703f8b038bf819f4a03c0dc248b
LukasPio <lucaspio.galvao@gmail.com>
2024-05-10 22:26:53 -0300

    Rename class and add annotations.

--- src/main/java/com/lucas/Controller/ClientController.java
+++ src/main/java/com/lucas/Controller/ClientController.java
@@ -3,8 +3,16 @@ package com.lucas.Controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "/api/client")
 public class ClientController {
+    @GetMapping(path = "/hello")
+    public ResponseEntity<String> helloWorld() {
+        System.out.println("get mapping");
+        return ResponseEntity.status(HttpStatus.OK).body("Augusto é viado");
+    }
+
 }
--- src/main/java/com/lucas/Model/ClientModel.java
+++ src/main/java/com/lucas/Model/ClientModel.java
@@ -1,9 +1,25 @@
 package com.lucas.Model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Date;
+import java.sql.Timestamp;
 
 @Entity
-@Table(schema = "clients")
+@Table(name = "clients")
+@Getter
+@Setter
 public class ClientModel {
+    @Id
+    private Long id;
+    private String name;
+    private String surname;
+    private String email;
+    private Date birthdate;
+    private Timestamp created_at;
+    private Timestamp update_at;
 }

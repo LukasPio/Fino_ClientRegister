e6d681dc73f46b05151d5fe1eec317fb7190e876
LukasPio <lucaspio.galvao@gmail.com>
2024-05-14 21:34:58 -0300

    Add generation strategy to id

--- src/main/java/com/lucas/clientregister/Model/DisabledClientModel.java
+++ src/main/java/com/lucas/clientregister/Model/DisabledClientModel.java
@@ -1,8 +1,6 @@
 package com.lucas.clientregister.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,6 +17,7 @@ import java.sql.Timestamp;
 @Setter
 public class DisabledClientModel {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String surname;

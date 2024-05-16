4811f268979d2363810ea70d0400f2c4f864f033
LukasPio <lucaspio.galvao@gmail.com>
2024-05-16 10:16:54 -0300

    Change return type to Json

--- src/main/java/com/lucas/clientregister/Controller/ClientController.java
+++ src/main/java/com/lucas/clientregister/Controller/ClientController.java
@@ -1,14 +1,11 @@
 package com.lucas.clientregister.Controller;
 
 import com.lucas.clientregister.DTO.ClientRequestDTO;
-import com.lucas.clientregister.DTO.ClientResponseDTO;
-import com.lucas.clientregister.DTO.DisabledClientResponseDTO;
+import com.lucas.clientregister.Json;
 import com.lucas.clientregister.Service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/clients")
@@ -17,23 +14,23 @@ public class ClientController {
     private ClientService clientService;
 
     @GetMapping(path = "/disabled")
-    public ResponseEntity<List<DisabledClientResponseDTO>> getAllDisabledClients() {
+    public ResponseEntity<Json> getAllDisabledClients() {
         return clientService.getAllDisabledClients();
     }
     @GetMapping
-    public ResponseEntity<List<ClientResponseDTO>> getAllClients() {
+    public ResponseEntity<Json> getAllClients() {
         return clientService.getAllClients();
     }
     @PostMapping
-    public ResponseEntity<String> saveClient(@RequestBody ClientRequestDTO clientData) {
+    public ResponseEntity<Json> saveClient(@RequestBody ClientRequestDTO clientData) {
         return clientService.saveClient(clientData);
     }
     @PutMapping(path = "/{email}")
-    public ResponseEntity<String> updateClient(@PathVariable String email, @RequestBody ClientRequestDTO clientData) {
+    public ResponseEntity<Json> updateClient(@PathVariable String email, @RequestBody ClientRequestDTO clientData) {
         return clientService.updateClient(clientData, email);
     }
     @DeleteMapping ("/{email}")
-    public ResponseEntity<String> deleteClient(@PathVariable String email) {
+    public ResponseEntity<Json> deleteClient(@PathVariable String email) {
         return clientService.deleteClient(email);
     }
 }

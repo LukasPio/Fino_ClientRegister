a4b75d2b7c03a5bdd69cf3a4c3b36659b3840fad
LukasPio <lucaspio.galvao@gmail.com>
2024-05-16 10:17:15 -0300

    Change return type to Json

--- src/main/java/com/lucas/clientregister/Service/ClientService.java
+++ src/main/java/com/lucas/clientregister/Service/ClientService.java
@@ -3,6 +3,7 @@ package com.lucas.clientregister.Service;
 import com.lucas.clientregister.DTO.ClientRequestDTO;
 import com.lucas.clientregister.DTO.ClientResponseDTO;
 import com.lucas.clientregister.DTO.DisabledClientResponseDTO;
+import com.lucas.clientregister.Json;
 import com.lucas.clientregister.Model.DisabledClientModel;
 import com.lucas.clientregister.Repository.DisabledClientRepository;
 import com.lucas.clientregister.logs.Logger;
@@ -13,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -25,66 +25,135 @@ public class ClientService {
     @Autowired
     private DisabledClientRepository disabledClientRepository;
 
-    public ResponseEntity<String> saveClient(ClientRequestDTO clientData) {
+    public ResponseEntity<Json> saveClient(ClientRequestDTO clientData) {
+        long elapsedTime = System.currentTimeMillis();
+        String elapsedTimeFormatted;
         if (clientRepository.existsByEmail(clientData.email())){
             Logger.warn("Trying save a user with email: "+clientData.email()+" but already is registered");
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Email: "+clientData.email()+" already is registered");
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(
+                    new Json(
+                            "Email: "+clientData.email()+" already is registered",
+                            String.valueOf(System.currentTimeMillis() - elapsedTime),
+                            "409 - Conflict"
+                            )
+            );
         }
         if (disabledClientRepository.existsByEmail(clientData.email())) {
             Logger.warn("Trying save a user with email: "+clientData.email()+" but is disabled");
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Email: "+clientData.email()+" is disabled contact our support");
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(
+                    new Json(
+                            "Email: "+clientData.email()+" is disabled contact our support",
+                            String.valueOf(System.currentTimeMillis() - elapsedTime),
+                            "401 - Unauthorized"
+                    )
+            );
         }
         clientRepository.save(new ClientModel(clientData));
         Logger.info("Client with email: "+clientData.email()+" was registered successfully");
-        return ResponseEntity.status(HttpStatus.CREATED).body("Client was registered successfully");
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                new Json(
+                        "Client was registered successfully",
+                        String.valueOf(System.currentTimeMillis() - elapsedTime)
+                )
+        );
     }
 
-    public ResponseEntity<String> updateClient(ClientRequestDTO clientData, String email) {
+    public ResponseEntity<Json> updateClient(ClientRequestDTO clientData, String email) {
+        long elapsedTime = System.currentTimeMillis();
         Optional<ClientModel> optionalClient = clientRepository.findByEmail(email);
         if (optionalClient.isEmpty())
         {
             Logger.warn("Trying update client with email: "+email+" but is not registered");
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Email: "+email+" is not registered");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                    new Json(
+                            "Email: "+email+" is not registered",
+                            String.valueOf(System.currentTimeMillis() - elapsedTime),
+                            "404 - Not Found"
+                    )
+            );
         }
         ClientModel client = optionalClient.get();
         client.update(clientData);
         clientRepository.save(client);
         Logger.info("User with email: "+email+" was updated successfully. new email: "+clientData.email());
-        return ResponseEntity.status(HttpStatus.OK).body("User was updated successfully");
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new Json(
+                        "User was updated successfully",
+                        String.valueOf(System.currentTimeMillis() - elapsedTime)
+                )
+        );
     }
 
     @Transactional
-    public ResponseEntity<String> deleteClient(String email) {
+    public ResponseEntity<Json> deleteClient(String email) {
+        long elapsedTime = System.currentTimeMillis();
         Optional<ClientModel> clientModel = clientRepository.findByEmail(email);
         if (clientModel.isEmpty()) {
             Logger.warn("Trying delete client with email: " + email + "but not registered");
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Email: " + email + " is not registered");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                    new Json(
+                            "Email: " + email + " is not registered",
+                            String.valueOf(System.currentTimeMillis() - elapsedTime),
+                            "404 - Not Found"
+                    )
+            );
         }
         clientRepository.deleteByEmail(email);
         disabledClientRepository.save(new DisabledClientModel(clientModel.get()));
         Logger.info("Client with email: "+email+" was deleted successfully");
-        return ResponseEntity.status(HttpStatus.OK).body("Client with email: "+email+" was deleted successfully");
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new Json(
+                        "Client with email: "+email+" was deleted successfully",
+                        String.valueOf(System.currentTimeMillis() - elapsedTime)
+                )
+        );
     }
 
-    public ResponseEntity<List<ClientResponseDTO>> getAllClients() {
+    public ResponseEntity<Json> getAllClients() {
+        long elapsedTime = System.currentTimeMillis();
         List<ClientModel> clientsModels = clientRepository.findAll();
         List<ClientResponseDTO> clients = clientsModels.stream().map(ClientResponseDTO::new).toList();
         if (clientsModels.isEmpty()) {
             Logger.warn("Trying to view all clients, but none registered");
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(clients);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                    new Json(
+                            "None clients are registered",
+                            String.valueOf(System.currentTimeMillis() - elapsedTime),
+                            "404 - Not Found"
+                    )
+            );
         }
         Logger.info("Getting all clients of database");
-        return ResponseEntity.status(HttpStatus.OK).body(clients);
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new Json(
+                        clients,
+                        "All clients get successfully",
+                        String.valueOf(System.currentTimeMillis() - elapsedTime)
+                )
+        );
     }
 
-    public ResponseEntity<List<DisabledClientResponseDTO>> getAllDisabledClients() {
+    public ResponseEntity<Json> getAllDisabledClients() {
+        long elapsedTime = System.currentTimeMillis();
         List<DisabledClientModel> disabledClients = disabledClientRepository.findAll();
         List<DisabledClientResponseDTO> disabledClientsData = disabledClients.stream().map(DisabledClientResponseDTO::new).toList();
         if (disabledClientsData.isEmpty()) {
             Logger.warn("Trying to view all disabled clients, but not have disabled clients");
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(disabledClientsData);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                    new Json(
+                            "None clients are disabled",
+                            String.valueOf(System.currentTimeMillis() - elapsedTime),
+                            "404 - Not Found"
+                    )
+            );
         }
         Logger.info("Getting all disable clients of database");
-        return ResponseEntity.status(HttpStatus.OK).body(disabledClientsData);
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new Json(
+                        disabledClientsData.toString(),
+                        "Getting all clients successfully",
+                        String.valueOf(System.currentTimeMillis() - elapsedTime)
+                )
+        );
     }
 }

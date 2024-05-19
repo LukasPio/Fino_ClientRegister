c728da51b88b0f3593e699edebab6619daf150dc
LukasPio <lucaspio.galvao@gmail.com>
2024-05-18 22:24:28 -0300

    Adapting constructors to new DTOs

--- src/main/java/com/lucas/clientregister/Json.java
+++ src/main/java/com/lucas/clientregister/Json.java
@@ -1,11 +1,13 @@
 package com.lucas.clientregister;
 
+import com.lucas.clientregister.DTO.ClientListResponseDTO;
 import com.lucas.clientregister.DTO.ClientResponseDTO;
+import com.lucas.clientregister.DTO.DisabledClientListResponseDTO;
 import lombok.Getter;
 import lombok.Setter;
+
 import java.sql.Timestamp;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Getter
@@ -26,11 +28,24 @@ public class Json {
         BODY.put("error", error);
     }
 
-    public Json(List<ClientResponseDTO> clientData, String message, String elapsedTime) {
-        BODY.put("clients", clientData);
+    public Json(ClientListResponseDTO clients, String message, String elapsedTime) {
+        BODY.put("clients", clients);
         BODY.put("message", message);
         BODY.put("elapsedTime", elapsedTime + "ms");
         BODY.put("timestamp", new Timestamp(System.currentTimeMillis()));
     }
 
+    public Json(DisabledClientListResponseDTO disabledClients, String message, String elapsedTime) {
+        BODY.put("clients", disabledClients);
+        BODY.put("message", message);
+        BODY.put("elapsedTime", elapsedTime + "ms");
+        BODY.put("timestamp", new Timestamp(System.currentTimeMillis()));
+    }
+
+    public Json(ClientResponseDTO client, String message, String elapsedTime) {
+        BODY.put("client", client);
+        BODY.put("message", message);
+        BODY.put("elapsedTime", elapsedTime + "ms");
+        BODY.put("timestamp", new Timestamp(System.currentTimeMillis()));
+    }
 }

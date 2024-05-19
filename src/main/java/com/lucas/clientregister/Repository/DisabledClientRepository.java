9fb38d28a347e10b727c43a0b8806c4d4d91c8c6
LukasPio <lucaspio.galvao@gmail.com>
2024-05-18 21:57:27 -0300

    Add delete by email

--- src/main/java/com/lucas/clientregister/Repository/DisabledClientRepository.java
+++ src/main/java/com/lucas/clientregister/Repository/DisabledClientRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface DisabledClientRepository extends JpaRepository<DisabledClientModel, Integer> {
     boolean existsByEmail(String email);
+    void deleteByEmail(String email);
 }
